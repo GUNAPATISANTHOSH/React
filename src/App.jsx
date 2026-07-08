5bec8624d651cc992ad04a486f18647840904936
GUNAPATISANTHOSH <santoshsantutej@gmail.com>
2026-07-08 11:42:45 +0530

    Your commit messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header';
-import Services from './components/Services';
+import Services from './components/Services'
+import Footer from './components/Footer';
 function App(){
 
   return(<div>
               <h1>App Component</h1>
               <Header/>
               <Services/>
+              <Footer/>
         </div>)
 }
 
