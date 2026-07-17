8f1f9f036f06be64632e6f579a5d0c87eebdf173
GUNAPATISANTHOSH <santoshsantutej@gmail.com>
2026-07-17 11:48:29 +0530

    fastapitable

--- src/Httpex/Effect.jsx
+++ src/Httpex/Effect.jsx
@@ -1,38 +1,45 @@
-import React, { useEffect, useState } from "react";
-import Axios from "axios";
-let Effect = () => {
-    let [users, setUsers] = useState([]);
-    useEffect(() => {
-        Axios.get("https://jsonplaceholder.typicode.com/users")
-            .then((resp) => {
-                setUsers(resp.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
-    return (
-        <React.Fragment>
-            <h3>User Component</h3>
-            <table border="3">
-                <thead>
-                    <tr>
-                        <th>User ID</th>
-                        <th>Name</th>
-                        <th>City</th>
+import { useEffect, useState } from "react";
+import axios from "axios";
+let Users=()=>{
+    let [users,setUsers]=useState([])
+    useEffect(()=>{
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then((resp)=>{
+            setUsers(resp.data);
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
+
+    },[]);
+    
+    return <div>
+        <h3>FastAPI</h3>
+        <table border={2}>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>Username</th>                     
+                    <th>Zipcode</th>                     
+                    <th>Phone</th>                     
+
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user)=>(
+                    <tr key={user.id} >
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.address.city}</td>
+                        <td>{user.username}</td>
+                        <td>{user.address.zipcode}</td> 
+                        <td>{user.phone}</td> 
                     </tr>
-                </thead>
-                <tbody>
-                    {users.map((user) => (
-                        <tr key={user.id}>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.address.city}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </React.Fragment>
-    );
-};
-export default Effect;
+                ))}
+            </tbody>
+        </table>
+    </div>
+}
+export default Users;
--- src/Httpex/users.jsx
+++ src/Httpex/users.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import axios from "axios";
 let Users=()=>{
-    let[users,setUsers]=useState([]);
+    let [users,setUsers]=useState([])
     useEffect(()=>{
         axios.get("https://jsonplaceholder.typicode.com/users")
         .then((resp)=>{
@@ -10,29 +10,36 @@ let Users=()=>{
         .catch((error)=>{
             console.log(error);
         });
+
     },[]);
-    return ( 
-        <div>
-            <h3>User Component</h3>
-            <table border={2}>
-                <thead>
-                    <tr>
-                        <th>id </th>
-                        <th>Name</th>
-                        <th>City</th>
+    
+    return <div>
+        <h3>FastAPI</h3>
+        <table border={2}>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>Username</th>                     
+                    <th>Zipcode</th>                     
+                    <th>Phone</th>                     
+
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user)=>(
+                    <tr key={user.id} >
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.address.city}</td>
+                        <td>{user.username}</td>
+                        <td>{user.address.zipcode}</td> 
+                        <td>{user.phone}</td> 
                     </tr>
-                </thead>
-                <tbody>
-                    {users.map((users)=>(
-                        <tr>
-                            <td>{users.id}</td>
-                            <td>{users.name}</td>
-                            <td>{users.address.city}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    )
+                ))}
+            </tbody>
+        </table>
+    </div>
 }
-export default Users;   
+export default Users;
