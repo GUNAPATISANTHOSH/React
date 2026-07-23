130e975dbb993b6760857e6e292ea820ee7e895f
GUNAPATISANTHOSH <santoshsantutej@gmail.com>
2026-07-23 17:21:40 +0530

    registration form handlin

--- practice/src/Registration/Register.jsx
+++ practice/src/Registration/Register.jsx
@@ -5,7 +5,25 @@ let Register =()=>{
     let usernameHandler=(event)=>{
         setUser({...user,username:event.target.value})
     }
+    let mobileHandler=(event)=>{
+        setUser({...user,mobile:event.target.value})
+    }
+    let dateHandler=(event)=>{
+        setUser({...user,date:event.target.value})
+    }
+    let genderHandler=()=>{
+        setUser({...user,gender:event.target.value})
+    }
+    let stateHandler = (event) => {
+    console.log(event.target.value);
+
+    setUser({...user,state: event.target.value});
+    };
+    let messageHandler=()=>{
+        setUser({...user,textbox:event.target.value})
+    }
     let registerHandler=()=>{
+        event.preventDefault()
         alert("login successful")
     }
     return <div class="container mt-5">
@@ -16,27 +34,25 @@ let Register =()=>{
             <form onSubmit={registerHandler}>
                 <pre>{JSON.stringify(user)}</pre>
                 User Name:<input type="username" onChange={usernameHandler} /> <br /><br />
-                Mobile No:<input type="Mobile "/> <br /><br />
-                DOB:::::::<input type="date"/><br /><br />
-                Gender :::<input type="radio" name="gender" id="Male"/>male
-                <input type="radio" name="gender" id="female"/>Female<br/> <br />
-                State:::::<select type="state" id="please select state" name="select">
-                    <option value="" selected disabled>---Select State---</option>
-                    <option value="">Andhra Pradesh</option>
-                    <option value="">Telangana</option>
-                    <option value="">Tamil Nadu</option>
-                    <option value="">Kerala</option>
+                Mobile No:<input type="Mobile " onChange={mobileHandler}/> <br /><br />
+                DOB:::::::<input type="date" onChange={dateHandler}/><br /><br />
+                Gender :::<input type="radio" name="gender" id="Male" value="Male" onChange={genderHandler}/>male
+                <input type="radio" name="gender" id="female" value="Female" onChange={genderHandler}/>Female<br/> <br />
+                State:::::<select name="state" value={user.state} onChange={stateHandler}>
+                <option value="" disabled> ---Select State---</option>
+                <option value="Andhra Pradesh">Andhra Pradesh</option>
+                <option value="Telangana">Telangana</option>
+                <option value="Tamil Nadu">Tamil Nadu</option>
+                <option value="Kerala">Kerala</option>
                 </select>
                 <br/><br />
-                Message:::<input type="textbox"/><br/><br />
+                Message:::<input type="textbox" onChange={messageHandler}/><br/><br />
                 <input type="checkbox" />Please Accept  t&c <br/><br />
                 <input type="submit" value="Registration"/> 
                    
                 <input type="submit" value="Cancel"/>
             </form>
-
             </div>
-
         </div>
     </div>
 </div>
